70d84c422210aa124d2321dd6f867f705cedf87c
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-04-26 17:55:33 +0200

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+// Dichiarazione variabili
+
+let firstName;
+let lastName;
+let favColour;
+let password;
+
+
+
 // Nome dell'utente
+firstName = prompt("Inserisci il tuo nome:");
+console.log(firstName);
 
 // Cognome dell'utente
+lastName = prompt("Inserisci il tuo Cognome:");
+console.log(lastName);
 
 // Colore preferito
+favColour = prompt("Inserisci il tuo colore preferito:");
+console.log(favColour);
 
 // Risultato
+password = firstName + lastName + favColour +21;
+console.log(password);
+
+
+document.getElementById("pass").innerHTML = password;
